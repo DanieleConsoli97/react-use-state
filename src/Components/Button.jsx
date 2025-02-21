2543ef698be1355e97d836ab2d8db62011ab8478
DanieleConsoli97 <Danyconsoli4@gmail.com>
2025-02-21 18:10:34 +0100

    end

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,9 @@
+import {useState} from 'react';
 function Button (props) {
   const  {id,title} = props;
   return(
-    
-    
-          <button key={id}>{title}</button>
-    
+          <button key={id} onClick={setClick}>{title}</button>
+
     )
 }
 export default Button;
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,22 +1,26 @@
-import Button from './Button';
-function Main (props) {
-    const {languages} = props;
+import { useState } from 'react';
+function Main(props) {
+        // Crea una variabile di stato 'click' inizializzata come stringa vuota
+    // setClick è la funzione per aggiornare lo stato 'click'
+    const [click, setClick] = useState("");
+    const { languages } = props;
     return (
         <main>
             <div className="languages">
-            {
-             
-              languages.map((element)=>{
-              const {id,title} = element;
-                
-                return( <Button key={id} title={title} /> )  
-              })
-            }
-            
+                {
+                    languages.map((element) => {
+                        const { id, title, description } = element;
+                        const clickon = () => { setClick(description) };
+                        return (<button key={id} onClick={clickon}>{title}</button>)
+                    })
+                }
             </div>
             <div className="description">
+                
+                    <p>{click}</p>
+                
             </div>
         </main>
     )
-} 
+}
 export default Main;
